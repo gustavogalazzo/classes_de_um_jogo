1c4120290b6dbd83b8176017f5d53fd0edcdd108
gustavogalazzo <gustavogalazzo2000@gmail.com>
2024-03-19 15:50:13 -0300

    Atualizando o 3 desafio de logica de programacao

--- class_game.js
+++ class_game.js
@@ -1,3 +1,4 @@
+// Criei uma class chamada "hero" na qual tem um constructor que tem os seguintes parametros : "name, type, weapon".
 class hero {
   constructor(name, type, weapon) {
     this.name = name
@@ -5,6 +6,7 @@ class hero {
     this.weapon = weapon
   }
 
+  // Já aqui defini um metodo na class, chamada "atacar" para imprimir os dados do heroi
   atacar() {
     console.log(
       `O heroi chamado ${this.name}, do tipo ${this.type}, atacou os inimigos com uma ${this.weapon}!`
@@ -12,11 +14,13 @@ class hero {
   }
 }
 
+//E para utilizar os métodos da class hero, crei uma instância chamada "new hero"
 var hero1 = new hero('Dead Sage', 'Guerreiro', 'Espada')
 var hero2 = new hero('Merlin', 'Mago', 'Magia')
 var hero3 = new hero('Lady Axeman', 'Elfa', 'Arco e Flecha')
 var hero4 = new hero('Hinata', 'Ninja', 'Jutsu')
 
+//Já nessa parte final chamei o metodo "atacar()" com todos os dados dos herois que defini na instancia
 hero1.atacar()
 hero2.atacar()
 hero3.atacar()
